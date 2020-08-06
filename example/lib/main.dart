c38122db1b05afd3add1fd441d377d6caa508501
Andrew Chen <yongjhih@gmail.com>
2020-08-06 09:14:51 +0800

    Upgrade SDK

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -216,7 +216,7 @@ class Item extends StatelessWidget {
                     padding:
                         EdgeInsets.symmetric(vertical: 14.0, horizontal: 14.0),
                     child: Text(data.title,
-                        style: Theme.of(context).textTheme.subtitle1),
+                        style: Theme.of(context).textTheme.subtitle),
                   )),
                   // Triggers the reordering
                   dragHandle,
